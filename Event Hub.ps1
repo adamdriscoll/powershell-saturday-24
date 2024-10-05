3c5f94714655d46b182f7645eb6f912b6815bd8b
admin <admin>
2024-10-05 10:18:33 -0500

    Event Hubs.

--- Event Hub.ps1
+++ Event Hub.ps1
@@ -1,3 +1,9 @@
-﻿Invoke-PSUCommand -Command "New-Item" -Parameters @{
-    Path = "/home/usr/desktop/something.txt"
+﻿<#
+    $ENV:PSU_Connections__0__Url = 'http://localhost:5000'
+    $ENV:PSU_Connections__0__Hub = 'Linux'
+#>
+
+Invoke-PSUCommand -Command "New-Item" -Parameters @{
+    Path = "/usr/share/$(Get-Random).txt"
+    ItemType = 'File'
 } -Hub Linux
